0b141592369d8904c78ccb70dd692feb690784a5
syedabuabakarali70 <syedabubakarali70@gmail.com>
2024-03-18 13:37:21 +0500

    Created Topic cards

--- src/app/dsa/page.tsx
+++ src/app/dsa/page.tsx
@@ -1,12 +1,61 @@
 import TopicCard from '@/components/TopicCard'
 
+const links =[
+  {
+    name:"Bubble Sort",
+    link:"bubblesort",
+  },
+  {
+    name:"Merge Sort",
+    link:"mergesort",
+  },
+  {
+    name:"Quick Sort",
+    link:"quicksort",
+  },
+  {
+    name:"Insertion Sort",
+    link:"insertionsort",
+  },
+  {
+    name:"Counting Sort",
+    link:"countingsort",
+  },
+  {
+    name:"Selection Sort",
+    link:"selectionsort",
+  },
+  {
+    name:"Arays",
+    link:"arrays",
+  },
+  {
+    name:"Stack",
+    link:"stack",
+  },
+  {
+    name:"Queue",
+    link:"queue",
+  },
+  {
+    name:"Linked List",
+    link:"linkedlist",
+  },
+  {
+    name:"Tree",
+    link:"Trees",
+  },
+  
+]
+
 const DSA = () => {
   return (
-    <section className='flex border-2 m-2 p-2'>
-      <TopicCard/>
-      <TopicCard/>
-      <TopicCard/>
-      <TopicCard/>
+    <section className='flex border-2 m-2 p-2 flex-wrap gap-2 justify-around'>
+      {links.map((link,index)=>(
+        <TopicCard info={link} key={index}/>
+
+      ))}
+      
     </section>
   )
 }
