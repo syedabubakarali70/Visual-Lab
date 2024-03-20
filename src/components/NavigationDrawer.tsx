8fa9776ad0de601439f993af2211189e1799d8c7
syedabuabakarali70 <syedabubakarali70@gmail.com>
2024-03-20 22:44:45 +0500

    Drawer improvement

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -21,7 +21,7 @@ const NavigationDrawer = () => {
       <DrawerTrigger className="cursor-pointer hover:bg-background w-12 h-12 flex justify-center items-center rounded-full">
           <HamburgerMenuIcon width="24" height="24" onClick={handleOpen}/>
       </DrawerTrigger>
-      <DrawerContent className="top-16 left-0 right-0 mt-0 w-full rounded-none h-min">
+      <DrawerContent className="top-0 left-0 right-0 mt-0 w-full rounded-none h-min-screen">
             <Link href="/dsa" onClick={handleOpen}>DSA</Link>
             <Link href="/texteditor" onClick={handleOpen}>Text Editor</Link>
           <DrawerClose asChild>
