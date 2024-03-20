0fbb45b77c53762fed8f7915e30b95ecdb8fe995
syedabuabakarali70 <syedabubakarali70@gmail.com>
2024-03-20 22:41:25 +0500

    Drawer improvement

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -15,16 +15,17 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 const NavigationDrawer = () => {
   const [open, setOpen] = useState(false);
+  const handleOpen = ()=> setOpen(!open)
   return (
-    <Drawer direction="top" open={open}>
+    <Drawer direction="top" open={open} onDrag={handleOpen} onRelease={handleOpen}>
       <DrawerTrigger className="cursor-pointer hover:bg-background w-12 h-12 flex justify-center items-center rounded-full">
-          <HamburgerMenuIcon width="24" height="24" onClick={()=>setOpen(!open)}/>
+          <HamburgerMenuIcon width="24" height="24" onClick={handleOpen}/>
       </DrawerTrigger>
       <DrawerContent className="top-16 left-0 right-0 mt-0 w-full rounded-none h-min">
-            <Link href="/dsa" onClick={()=>setOpen(!open)}>DSA</Link>
-            <Link href="/texteditor" onClick={()=>setOpen(!open)}>Text Editor</Link>
+            <Link href="/dsa" onClick={handleOpen}>DSA</Link>
+            <Link href="/texteditor" onClick={handleOpen}>Text Editor</Link>
           <DrawerClose asChild>
-            <Button onClick={()=>setOpen(!open)}>Close</Button>
+            <Button onClick={handleOpen}>Close</Button>
           </DrawerClose>
       </DrawerContent>
     </Drawer>
