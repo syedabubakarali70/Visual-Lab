53027d4edd3a697db6c48c29deea80e78643827f
syedabuabakarali70 <syedabubakarali70@gmail.com>
2024-06-06 09:46:44 +0500

    chat messages

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,7 +14,7 @@ const Message = ({
     return (
       <div className="flex flex-row-reverse items-start">
         <div className="rounded-full bg-secondary p-2 w-8 h-8 flex justify-center items-center ml-1 mt-2">
-          {userName[0]}
+          {userName?.[0]}
         </div>
         <div className="bg-secondary max-w-[70%] px-3 py-2 rounded-lg my-1">
           {msg.text}
@@ -26,7 +26,7 @@ const Message = ({
     return (
       <div className="flex">
         <div className="rounded-full bg-blue-700 p-2 w-8 h-8 flex justify-center items-center ml-1 mt-2">
-          {userName[0]}
+          {userName?.[0]}
         </div>
         <div className="bg-blue-700 max-w-[70%] px-3 py-2 rounded-lg my-1">
           {msg.text}
