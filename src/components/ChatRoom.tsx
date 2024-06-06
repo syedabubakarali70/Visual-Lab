2a1bf78a8c297e221877f038597b6cdc269d508b
syedabuabakarali70 <syedabubakarali70@gmail.com>
2024-06-06 09:41:40 +0500

    chat messages

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -9,7 +9,6 @@ import {
 } from "firebase/firestore";
 import {
   useCollection,
-  useCollectionData,
 } from "react-firebase-hooks/firestore";
 import Message from "./Message";
 import { UserAuth } from "@/app/context/AuthContext";
@@ -19,35 +18,39 @@ import { useRef, useState } from "react";
 const ChatRoom = ({ roomId }: { roomId: string }) => {
   const { user } = UserAuth();
   const dummy = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<any>(null);
   const q = query(
     collection(db, "rooms", roomId, "RoomChat"),
     orderBy("createdAt")
   );
-  const [messages, loading, error] = useCollection(q);
-  const [sendMessage, setSendMessage] = useState("Enter message");
-  console.log(messages);
+const [messages, loading, error] = useCollection(q);
+const [sendMessage, setSendMessage] = useState("Enter message");
 
-  const handleSendMessage = async () => {
-    await addDoc(collection(db, "rooms", roomId, "RoomChat"), {
-      uid: user.uid,
-      text: sendMessage,
-      createdAt: serverTimestamp(),
-    });
-    setSendMessage("");
+
+const handleSendMessage = async () => {
+    inputRef.current && (inputRef.current.value = ""); 
     dummy.current?.scrollIntoView({ behavior: "smooth" });
-  };
+    setSendMessage("");
+    await addDoc(collection(db, "rooms", roomId, "RoomChat"), {
+        uid: user.uid,
+        text: sendMessage,
+        createdAt: serverTimestamp(),
+        sender: user.displayName,
+    });
+};
 
   return (
-    <div className="w-[40%] fixed bottom-4 right-4 h-96 border-2 rounded-md p-2 flex flex-col justify-between">
-      <div className="relative">
+    <div className="w-full md:w-[40%] fixed bottom-4 right-4 h-96 border-2 rounded-md p-2 flex flex-col justify-between">
+      <div className="overflow-y-scroll">
         {messages?.docs.map((message) => (
-          <Message key={message.id} msg={message.data()} uid={user.uid} />
+          <Message key={message.id} msg={message.data()} uid={user.uid} userName={message.data().sender}/>
         ))}
       </div>
       <div ref={dummy}></div>
-      <div className="flex w-full items-center space-x-2">
+      <div className="flex w-full items-center space-x-2 pt-2">
         <Input
           type="text"
+          ref={inputRef}
           placeholder={sendMessage}
           onChange={(e) => setSendMessage(e.target.value)}
           onKeyDown={(e) => {
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 
-const Message = ({ msg, uid }: { msg: any; uid: string }) => {
+const Message = ({
+  msg,
+  uid,
+  userName,
+}: {
+  msg: any;
+  uid: string;
+  userName: string;
+}) => {
   if (uid === msg.uid) {
     //sent
     return (
-      <div className="flex flex-row-reverse">
-        <div className="bg-secondary max-w-[70%] px-3 py-2 rounded-lg my-2  right-0">
+      <div className="flex flex-row-reverse items-start">
+        <div className="rounded-full bg-secondary p-2 w-8 h-8 flex justify-center items-center ml-1 mt-2">
+          {userName[0]}
+        </div>
+        <div className="bg-secondary max-w-[70%] px-3 py-2 rounded-lg my-1">
           {msg.text}
         </div>
       </div>
     );
   } else {
     //received
-    return <div className="flex">
-    <div className="bg-blue-700 max-w-[70%] px-3 py-2 rounded-lg my-2  right-0">
-      {msg.text}
-    </div>
-  </div>;
+    return (
+      <div className="flex">
+        <div className="rounded-full bg-blue-700 p-2 w-8 h-8 flex justify-center items-center ml-1 mt-2">
+          {userName[0]}
+        </div>
+        <div className="bg-blue-700 max-w-[70%] px-3 py-2 rounded-lg my-1">
+          {msg.text}
+        </div>
+      </div>
+    );
   }
 };
 
