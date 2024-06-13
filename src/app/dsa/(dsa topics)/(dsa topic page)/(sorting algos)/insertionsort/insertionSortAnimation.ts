5d7d12b1d760b92f7c03a2b145809a66a377bb28
syedabuabakarali70 <syedabubakarali70@gmail.com>
2024-06-13 11:16:09 +0500

    Added quick and insertion sort

--- src/app/dsa/(dsa topics)/(dsa topic page)/(sorting algos)/insertionsort/insertionSortAnimation.ts
+++ src/app/dsa/(dsa topics)/(dsa topic page)/(sorting algos)/insertionsort/insertionSortAnimation.ts
@@ -1,47 +1,42 @@
 import { Inumbers } from "@/components/useCreateArray";
-import { on } from "events";
 const width = 49.6;
 function insertionSort(timeline: any, array: Inumbers[], numbersRef: any) {
   for (let i = 1; i < array.length; i++) {
-    let key = array[i].value;
+    let key = { ...array[i] };
     let j = i - 1;
     timeline.to(numbersRef.current[array[i].index].span, {
       y: "+=40",
-      borderColor: "red",
-      borderWidth: 2,
       duration: 0.5,
     });
-    // Move elements of array[0..i-1], that are greater than key,
-    // to one position ahead of their current position
-    console.log("itr = ",i)
-    while (j >= 0 && array[j].value > key) {
+    while (j >= 0 && array[j].value > key.value) {
       timeline.to(numbersRef.current[array[j].index].span, {
         x: "+=" + width,
-        borderColor: "yellow",
-      borderWidth: 2,
+        borderColor: "red",
+        borderWidth: 2,
         duration: 0.5,
-        onStart:console.log(j)
       });
-      timeline.to(numbersRef.current[array[j].index].span, {
-      borderWidth: 0,
+      timeline.set(numbersRef.current[array[j].index].span, {
+        borderWidth: 0,
       });
-      array[j + 1].value = array[j].value;
+      array[j + 1] = { ...array[j] };
       j = j - 1;
     }
-    array[j + 1].value = key;
+    array[j + 1] = { ...key };
     const xValue = () => {
-      let temp = (i) * width;
+      let temp = (i - j - 1) * width;
       return "-=" + temp.toString();
     };
-    timeline.to(numbersRef.current[array[i].index].span, {
+    timeline.to(numbersRef.current[key.index].span, {
       x: xValue(),
       duration: 0.5,
     });
-    timeline.to(numbersRef.current[array[i].index].span, {
+    timeline.to(numbersRef.current[key.index].span, {
       y: 0,
-      borderWidth:0,
       duration: 0.2,
     });
   }
+  array.map((num) => {
+    console.log(num.value, num.index);
+  });
 }
 export default insertionSort;
